074b48132485b095cf77fb7cf3606aeadbc1c0de
cindyanorris <cindyanorris@gmail.com>
2017-11-30 15:43:14 -0500

    Update defs.h

--- defs.h
+++ defs.h
@@ -3,7 +3,7 @@
 typedef struct
 {
    int * indices;  //holds the row and column indices for waldo locations
-   int count;  //number of elements in the indices array (2 * number of waldos)
+   int count;  //number of rows and columns stored in the indices array (2 * number of waldos)
    int size;   //size of the indices array (size >= count)
 } locationType;
 
